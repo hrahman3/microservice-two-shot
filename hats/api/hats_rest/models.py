8efffc87b19a7d7e19aa80e722722e06d59cee6c
Hamzah <hamzahrahman123@gmail.com>
2023-08-29 17:54:58 -0400

    Polls, and Models Done

--- hats/api/hats_rest/models.py
+++ hats/api/hats_rest/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+# from django.urls import reverse
 
 # Create your models here.
+class LocationVO(models.Model):
+    import_href = models.CharField(max_length=200, unique=True)
+    closet_name = models.CharField(max_length=200)
+    shelf_number = models.PositiveIntegerField(blank=True, null=True)
+    section_number = models.PositiveIntegerField(blank=True, null=True)
+
+
+
+
+
+class Hat(models.Model):
+    fabric = models.CharField(max_length=200)
+    style_name = models.CharField(max_length=200)
+    color = models.CharField(max_length=200)
+    image_url = models.URLField()
+
+    location = models.ForeignKey(
+        LocationVO,
+        related_name="hats",
+        on_delete=models.CASCADE,
+    )
--- hats/api/hats_rest/views.py
+++ hats/api/hats_rest/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+import json
+from common.json import ModelEncoder
+from .models import LocationVO, Hat
+
 
 # Create your views here.
+
+class LocationVODetailEncoder(ModelEncoder):
+    model = LocationVO
+    properties = [
+        "import_href",
+        "closet_name",
+        "shelf_number",
+        "section_number",
+]
+
+
+
+class HatListEncoder(ModelEncoder):
+    model = Hat
+    properties = [
+        "id",
+        "fabric",
+        "style_name",
+        "color",
+        "hat"
+    ]
