48677f9e5c6d6434f4adec8848b865ea9f042abb
Hamzah <hamzahrahman123@gmail.com>
2023-08-29 23:09:57 -0400

    api views complete

--- hats/api/hats_rest/views.py
+++ hats/api/hats_rest/views.py
@@ -21,10 +21,69 @@ class LocationVODetailEncoder(ModelEncoder):
 
 class HatListEncoder(ModelEncoder):
     model = Hat
+    properties = ["style_name"]
+
+    def get_extra_data(self, o):
+        return {"location": o.location.closet_name}
+
+
+
+class HatDetailEncoder(ModelEncoder):
+    model = Hat
     properties = [
-        "id",
-        "fabric",
+        "image_url",
         "style_name",
         "color",
-        "hat"
+        "fabric",
+        "location",
     ]
+    encoders = {
+        "location": LocationVODetailEncoder(),
+    }
+
+
+@require_http_methods(["GET", "POST"])
+def api_hat_list(request, location_vo_id=None):
+    if request.method =="GET":
+        if location_vo_id is not None:
+            hats = Hat.objects.filter(location=location_vo_id)
+        else:
+            hats = Hat.objects.all()
+        return JsonResponse(
+            {"hats": hats},
+            encoder=HatListEncoder,
+
+        )
+    else:
+        content = json.loads(request.body)
+
+        try:
+            location_id = content["location"]
+            location = LocationVO.objects.get(id=location_id)
+            content["location"] = location
+        except LocationVO.DoesNotExist:
+            return JsonResponse(
+                {"message": "Invalid location id"},
+                status=400,
+            )
+
+        hat = Hat.objects.create(**content)
+        return JsonResponse(
+            hat,
+            encoder=HatDetailEncoder,
+            safe=False,
+        )
+
+
+@require_http_methods(["GET", "DELETE"])
+def api_hat_detail(request, pk):
+    if request.method == "GET":
+        hat = Hat.objects.get(id=pk)
+        return JsonResponse(
+            {"hat": hat},
+            encoder=HatDetailEncoder,
+            safe=False,
+        )
+    else:
+        count, _ = Hat.objects.filter(id=pk).delete()
+        return JsonResponse({"deleted": count > 0})
